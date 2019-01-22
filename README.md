9912fc91d63edbd219420bc59df1b37d5dcd2df1
William Verde <williamverde10@gmail.com>
2019-01-22 17:18:04 -0400

    delete files

--- README.md
+++
@@ -1,2 +0,0 @@
-# asgardcms-icommercelocaldelivery
-
--- Repositories/ConfiglocaldeliveryRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\IcommerceLocaldelivery\Repositories;
-
-use Modules\Core\Repositories\BaseRepository;
-
-interface ConfiglocaldeliveryRepository extends BaseRepository
-{
-}
